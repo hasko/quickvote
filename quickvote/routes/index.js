3246018756dd08308e2abd33012ceeedc167acea
Hasko <hasko@heinecke.com>
2016-11-19 15:42:25 +0000

    Simple display of question and simple error handling

--- quickvote/routes/index.js
+++ quickvote/routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
 
+var client = require('redis').createClient();
+
+client.on('error', function (err) {
+  console.log('Error ' + err);
+});
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  client.get('current_vote', function (err, reply) {
+    console.log(reply);
+    if (err || reply == null) {
+      if (err) {
+        console.log(err);
+      }
+      res.render('nocurrent');
+    } else {
+      res.render('index', { title: 'QuickVote', question: reply });
+    }
+  });
 });
 
 module.exports = router;
