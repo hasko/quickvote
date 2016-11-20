03a5a642d60bf2dfa53b31824926665eb86da42a
Hasko <hasko@heinecke.com>
2016-11-20 10:12:02 +0000

    Added support for multiple votes

--- quickvote/routes/index.js
+++ quickvote/routes/index.js
@@ -9,16 +9,25 @@ client.on('error', function (err) {
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  client.get('current_vote', function (err, reply) {
-    if (err || reply == null) {
+  client.setnx("vote:next.id", 0);
+  client.get("vote:current.id", function (err, curId) {
+    if (err || curId == null) {
       if (err) {
-        console.log(err);
+        console.log(err); // FIXME Use debug.
       }
       res.render('nocurrent');
     } else {
-      var vote = JSON.parse(reply);
-      console.log(vote);
-      res.render('index', { title: 'QuickVote', question: vote.q, answers: vote.a });
+      client.get('vote:' + curId, function (err, reply) {
+        if (err || reply == null) {
+          if (err) {
+            console.log(err); // FIXME Use debug.
+          }
+          res.render('nocurrent');
+        } else {
+          var vote = JSON.parse(reply);
+          res.render('index', { title: 'QuickVote', question: vote.q, answers: vote.a, action: "/vote" });
+        }
+      });
     }
   });
 });
